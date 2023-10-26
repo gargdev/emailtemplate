643a0f529e5d6c4f43e99026caf7448a9473623a
gargdev <anooppandey937@gmail.com>
2023-10-27 04:48:01 +0530

    implemented search functionality

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Categories from "./components/Categories";
 import SearchBar from "./components/SearchBar";
 import TemplateGrid from "./components/TemplateGrid";
-import { categories } from "./components/data";
+import { categories as allCategories } from "./components/data";
 import "./App.css";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredCategories = allCategories.filter((category) =>
+    category.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
   return (
     <>
-      <div className="background">
-        
-      </div>
+      <div className="background"></div>
       <div className="container">
-      <div className="header-section">
-        <h5>Marketplace</h5>
-        <h1 className="primary-heading mb-2">Email Templates</h1>
-        <h1 className="secondary-heading mb-2">
-          by the community, for the community.
-        </h1>
-        <p>
-          100s of free templates to help you craft the perfect marketing
-          journey.
-        </p>
-      </div>
+        <div className="header-section">
+          <h5>Marketplace</h5>
+          <h1 className="primary-heading mb-2">Email Templates</h1>
+          <h1 className="secondary-heading mb-2">
+            by the community, for the community.
+          </h1>
+          <p>100s of free templates to help you craft the perfect marketing journey.</p>
+        </div>
         <div className="hero-section">
-          <div className="hero-left">{<Categories />}</div>
+          <div className="hero-left">
+            
+            <Categories />
+          </div>
           <div className="hero-right">
             <div className="container srch-filter-section">
-            <SearchBar />
-            <div className="mb-4 filter">
-              <strong>Sort by: </strong>
-              <select className="ml-2 option">
-                <option>Recent</option>
-                <option>Popular</option>
-              </select>
+            <SearchBar onSearch={handleSearch} />
+              <div className="mb-4 filter">
+                <strong>Sort by: </strong>
+                <select className="ml-2 option">
+                  <option>Recent</option>
+                  <option>Popular</option>
+                </select>
+              </div>
             </div>
-            </div>
-            {categories.map((category) => (
+            {filteredCategories.map((category) => (
               <TemplateGrid key={category.name} category={category} />
             ))}
           </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import "../styles/searchbar.css"
-function SearchBar() {
-    return (
-        <input type="text" className="form-control mb-4 srch-bar" placeholder="Search email templates" />
-    );
+import React, { useState } from 'react';
+import "../styles/searchbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+function SearchBar({ onSearch }) {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchQuery);
+  };
+
+  return (
+    <div className="srch-bar">
+      <input
+        type="text"
+        className="form-control mb-4 srch-bar-input"
+        placeholder="Search email templates"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        onKeyUp={(e) => {
+          if (e.key === 'Enter') {
+            handleSearch();
+          }
+        }}
+      />
+    </div>
+  );
 }
 
 export default SearchBar;
